8ace19ae11ec2473b218845548c829083231f405
John Wallom <jwallom@gmail.com>
2025-12-02 16:34:25 -0500

    Add char counter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words
+from stats import get_unique_chars
 
 
 def get_book_text(file_path):
@@ -9,7 +10,9 @@ def get_book_text(file_path):
 def main():
     book_path = "./books/frankenstein.txt"
     book = get_book_text(book_path)
+    uniq_chars = get_unique_chars(book)
     print(f"Found {get_num_words(book)} total words")
+    print(uniq_chars)
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,12 @@
 def get_num_words(book):
     return len(book.split())
+
+
+def get_unique_chars(book):
+    char_count = {}
+    for char in book:
+        if char.lower() in char_count:
+            char_count[char.lower()] += 1
+        else:
+            char_count[char.lower()] = 1
+    return char_count
